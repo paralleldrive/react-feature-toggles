9ca21afd468533571d45924876151b56c7e01cbc
Kenny Lavender <leapingfish@gmail.com>
2017-08-12 09:22:55 -0700

    delete old components

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import FeatureToggles from "./feature-toggles";
-import FeatureEnabled from "./feature-enabled";
-import FeatureDisabled from "./feature-disabled";
-
-export default {
-  FeatureToggles,
-  FeatureEnabled,
-  FeatureDisabled
-};
