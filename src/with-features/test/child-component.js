bdca164b6e314ba8d37e8a03a50d49473f5133b4
Kenny Lavender <leapingfish@gmail.com>
2017-08-12 13:05:09 -0700

    delete garbage files

--- src/with-features/test/child-component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-const ChildComponent = (props, context) => (
-  <div className="child-component">{context.features.toString()}</div>
-);
-ChildComponent.contextTypes = { features: PropTypes.array };
-
-export default ChildComponent;
