ca2999c2067bfac740051a344c4fd389aed22d24
Kenny Lavender <leapingfish@gmail.com>
2017-08-12 19:26:46 -0700

    delete jsdom test fixtures

--- src/test/fixtures/find-by-class.js
+++
@@ -1,9 +0,0 @@
-import TestUtils from "react-dom/test-utils";
-
-const findByClass = (dom, selector) => {
-  const subject = selector.slice(1);
-
-  return TestUtils.findRenderedDOMComponentWithClass(dom, subject);
-};
-
-export default findByClass;
