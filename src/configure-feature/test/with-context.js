a5301716e19db6cc2b3a952b5f0d18f1a570cb32
thoragio <thoragio@gmail.com>
2017-08-17 01:47:54 +0200

    Setup pre-commit, more src linting

--- src/configure-feature/test/with-context.js
+++ src/configure-feature/test/with-context.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const withContext = (enabledFeatures = []) => WrappedComponent => {
   class ContextHOC extends Component {
