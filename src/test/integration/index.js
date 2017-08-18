55329b5b250a29cb71f1891ab2a03c0181d21eef
thoragio <thoragio@gmail.com>
2017-08-18 21:42:03 +0200

    Pass children to component wrapped by withFeatures()

--- src/test/integration/index.js
+++ src/test/integration/index.js
@@ -12,7 +12,7 @@ const render = ReactDOMServer.renderToStaticMarkup;
 Test Scenarios:
   no features
     no config
-      no fallbackComponent ✗
+      no fallbackComponent ✔
       with fallbackComponent
     empty config object
       no fallbackComponent
@@ -29,7 +29,7 @@ Test Scenarios:
 describe('integration of withFeatures() & configureFeature()', ({ test }) => {
   test('...no config (no features), no fallbackComponent', ({ end, deepEqual }) => {
     const NotFound = () => <div className="not-found">No help for you today!</div>;
-    const SomeComponent = () => <div></div>;
+    const SomeComponent = ({children} = {}) => <div>{children}</div>;
     const HelpChatComponent = () => <div className="help-chat">Need help? Call XXX-XXX-XXXX</div>;
     const Features = withFeatures()(SomeComponent);
     const ConfiguredFeature = configureFeature(NotFound)('help')(HelpChatComponent);
@@ -79,7 +79,7 @@ describe('integration of withFeatures() & configureFeature()', ({ test }) => {
     };
 
     const NotFound = () => <div className="not-found">No help for you today!</div>;
-    const SomeComponent = () => <div></div>;
+    const SomeComponent = ({children} = {}) => <div>{children}</div>;
     const HelpChatComponent = () => <div className="help-chat">Need help? Call XXX-XXX-XXXX</div>;
     const Features = withFeatures({ initialFeatures })(SomeComponent);
     const ConfiguredFeature = configureFeature(NotFound)('help')(HelpChatComponent);
@@ -111,58 +111,4 @@ describe('integration of withFeatures() & configureFeature()', ({ test }) => {
     }
     end();
   });
-
-  // test('...config with initialFeatures, no fallbackComponent, with props', ({ end, deepEqual }) => {
-  //   const initialFeatures = {
-  //     comments: {
-  //       enabled: false,
-  //       dependencies: []
-  //     },
-  //     help: {
-  //       enabled: true,
-  //       dependencies: []
-  //     },
-  //     sorting: {
-  //       enabled: false,
-  //       dependencies: []
-  //     }
-  //   };
-  //
-  //   const WrappedComponent = createWrappedComponent();
-  //   const FeatureComponent = withFeatures({ initialFeatures })(WrappedComponent);
-  //   const NotFound = createTestComponent('not-found');
-  //   const ConfiguredFeature = configureFeature(NotFound)('help')(FeatureComponent);
-  //   // const FeatureWithContext = withContext([])(ConfiguredFeature);
-  //
-  //   const someOtherProp = 'bacon and eggs';
-  //
-  //   const $ = dom.load(
-  //     render(<ConfiguredFeature someOtherProp={someOtherProp} />)
-  //   );
-  //   {
-  //     const msg = 'it should not render NotFound component';
-  //     const actual = $('.not-found').length;
-  //     const expected = 0;
-  //     deepEqual(actual, expected, msg);
-  //   }
-  //   {
-  //     const msg = 'it should render the Feature component';
-  //     const actual = $('.feature').length;
-  //     const expected = 1;
-  //     deepEqual(actual, expected, msg);
-  //   }
-  //   {
-  //     const msg = 'it should not render the Fallback component';
-  //     const actual = $('.fall-back').length;
-  //     const expected = 0;
-  //     deepEqual(actual, expected, msg);
-  //   }
-  //   {
-  //     const msg = 'it should pass through received props';
-  //     const actual = $('.some-other-prop').text();
-  //     const expected = someOtherProp;
-  //     deepEqual(actual, expected, msg);
-  //   }
-  //   end();
-  // });
 });
