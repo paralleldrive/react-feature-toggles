3187126321a14b3360edf2229797cd9529e7dad5
Kenny Lavender <leapingfish@gmail.com>
2018-01-21 09:19:34 -0800

    Refactor withFeatures test to riteway

--- src/test/with-features.js
+++ src/test/with-features.js
@@ -1,4 +1,4 @@
-import describe from 'tape';
+import { describe } from 'riteway';
 import withFeatures from '../with-features';
 import dom from 'cheerio';
 import React from 'react';
@@ -8,140 +8,141 @@ import createFeature from '../test-fixtures/create-feature';
 
 const render = ReactDOMServer.renderToStaticMarkup;
 
-describe('withFeatures()', ({ test }) => {
-  test('...no config', ({ end, deepEqual }) => {
-    const WrappedComponent = createWrappedComponent();
-    const Component = withFeatures()(WrappedComponent);
-    const $ = dom.load(render(<Component />));
-    {
-      const msg = 'the react context features should have no enabled features';
-      const actual = $('.context-features-string').text();
-      const expected = '';
-      deepEqual(actual, expected, msg);
-    }
-    {
-      const msg = 'the prop features should have no enabled features';
-      const actual = $('.props-features-string').text();
-      const expected = '';
-      deepEqual(actual, expected, msg);
-    }
+describe('withFeatures()', async should => {
+  const { assert } = should();
+  const WrappedComponent = createWrappedComponent();
+  const Component = withFeatures()(WrappedComponent);
+  const $ = dom.load(render(<Component />));
 
-    end();
+  assert({
+    given: 'the react context features',
+    should: 'have no enabled features',
+    actual: $('.context-features-string').text(),
+    expected: ''
   });
 
-  test('...empty config object', ({ end, deepEqual }) => {
-    const WrappedComponent = createWrappedComponent();
-    const Component = withFeatures({})(WrappedComponent);
-    const $ = dom.load(render(<Component />));
-    {
-      const msg = 'the react context features should have no enabled features';
-      const actual = $('.context-features-string').text();
-      const expected = '';
-      deepEqual(actual, expected, msg);
-    }
-    {
-      const msg = 'the prop features should have no enabled features';
-      const actual = $('.props-features-string').text();
-      const expected = '';
-      deepEqual(actual, expected, msg);
-    }
+  assert({
+    given: 'the prop features',
+    should: 'have no enabled features',
+    actual: $('.props-features-string').text(),
+    expected: ''
+  });
+});
 
-    end();
+describe('withFeatures({})', async should => {
+  const { assert } = should();
+  const WrappedComponent = createWrappedComponent();
+  const Component = withFeatures({})(WrappedComponent);
+  const $ = dom.load(render(<Component />));
+
+  assert({
+    given: 'the react context features',
+    should: 'have no enabled features',
+    actual: $('.context-features-string').text(),
+    expected: ''
   });
 
-  test('...config with initialFeatures', ({ end, deepEqual }) => {
-    const initialFeatures = [
-      createFeature({
-        name: 'help',
-        enabled: true,
-        dependencies: []
-      }),
-      createFeature({
-        name: 'comments',
-        enabled: true,
-        dependencies: []
-      }),
-      createFeature({
-        name: 'sorting',
-        enabled: false,
-        dependencies: []
-      })
-    ];
+  assert({
+    given: 'the prop features',
+    should: 'have no enabled features',
+    actual: $('.props-features-string').text(),
+    expected: ''
+  });
+});
 
-    const WrappedComponent = createWrappedComponent();
-    const Component = withFeatures({ initialFeatures })(WrappedComponent);
-    const $ = dom.load(render(<Component />));
-    {
-      const msg =
-        'the react context features should have the correct enabled features';
-      const actual = $('.context-features-string').text();
-      const expected = 'help,comments';
-      deepEqual(actual, expected, msg);
-    }
-    {
-      const msg = 'the prop features should have the correct enabled features';
-      const actual = $('.props-features-string').text();
-      const expected = 'help,comments';
-      deepEqual(actual, expected, msg);
-    }
+describe('withFeatures({ initialFeatures: [...Features] })', async should => {
+  const { assert } = should();
+  const initialFeatures = [
+    createFeature({
+      name: 'help',
+      enabled: true,
+      dependencies: []
+    }),
+    createFeature({
+      name: 'comments',
+      enabled: true,
+      dependencies: []
+    }),
+    createFeature({
+      name: 'sorting',
+      enabled: false,
+      dependencies: []
+    })
+  ];
 
-    end();
+  const WrappedComponent = createWrappedComponent();
+  const Component = withFeatures({ initialFeatures })(WrappedComponent);
+  const $ = dom.load(render(<Component />));
+
+  assert({
+    given: 'initial features',
+    should: 'set the correct react context correct enabled features',
+    actual: $('.context-features-string').text(),
+    expected: 'help,comments'
   });
 
-  test('...url search param overrides', ({ end, deepEqual }) => {
-    const initialFeatures = [
-      createFeature({
-        name: 'game',
-        enabled: false,
-        dependencies: []
-      }),
-      createFeature({
-        name: 'help',
-        enabled: false,
-        dependencies: []
-      }),
-      createFeature({
-        name: 'comments',
-        enabled: false,
-        dependencies: []
-      })
-    ];
+  assert({
+    given: 'initial features',
+    should: 'should the correct enabled features to the features props',
+    actual: $('.props-features-string').text(),
+    expected: 'help,comments'
+  });
+});
 
-    const WrappedComponent = createWrappedComponent();
-    const Component = withFeatures({
-      initialFeatures
-    })(WrappedComponent);
-    const $ = dom.load(render(<Component query={{ ft: 'game,comments' }} />));
-    {
-      const msg =
-        'the react context features should have the correct enabled features';
-      const actual = $('.context-features-string').text();
-      const expected = 'game,comments';
-      deepEqual(actual, expected, msg);
-    }
-    {
-      const msg = 'the prop features should have the correct enabled features';
-      const actual = $('.props-features-string').text();
-      const expected = 'game,comments';
-      deepEqual(actual, expected, msg);
-    }
+describe('withFeatures({ initialFeatures: [...Features] })({ query: {} })', async should => {
+  const { assert } = should();
+  const initialFeatures = [
+    createFeature({
+      name: 'game',
+      enabled: false,
+      dependencies: []
+    }),
+    createFeature({
+      name: 'help',
+      enabled: false,
+      dependencies: []
+    }),
+    createFeature({
+      name: 'comments',
+      enabled: false,
+      dependencies: []
+    })
+  ];
 
-    end();
+  const WrappedComponent = createWrappedComponent();
+  const Component = withFeatures({
+    initialFeatures
+  })(WrappedComponent);
+  const $ = dom.load(render(<Component query={{ ft: 'game,comments' }} />));
+
+  assert({
+    given: 'initial features and a query',
+    should: 'the react context should have the correct enabled features',
+    actual: $('.context-features-string').text(),
+    expected: 'game,comments'
   });
 
-  test('...received props', ({ end, deepEqual }) => {
-    const msg =
-      'it should pass through all received props to the wrapped component';
+  assert({
+    given: 'initial features and a query',
+    should: 'the correct enabled features to the features props',
+    actual: $('.props-features-string').text(),
+    expected: 'game,comments'
+  });
+});
 
+describe('withFeatures()({ ...props })', async should => {
+  const { assert } = should();
+  {
     const WrappedComponent = createWrappedComponent();
     const name = 'Joe Joe';
     const Component = withFeatures()(WrappedComponent);
     const $ = dom.load(render(<Component name={name} />));
 
-    const actual = $('.props-name').text();
-    const expected = name;
-    deepEqual(actual, expected, msg);
-
-    end();
-  });
+    assert({
+      given: 'props',
+      should: 'pass through props',
+      actual: $('.props-name').text(),
+      expected: name
+    });
+  }
 });
