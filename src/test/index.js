23f29e3bb4e4062eed26a515a7045102b4a3db8a
Kenny Lavender <leapingfish@gmail.com>
2018-05-12 15:19:19 -0700

    Update index.js

--- src/test/index.js
+++ src/test/index.js
@@ -5,6 +5,5 @@ import './update-features-with-query';
 import './with-features';
 import './configure-feature';
 import './is-feature-included';
-import './features';
 import './feature-toggles';
 import './feature';
