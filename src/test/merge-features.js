747850176e9d2e3d14b7062c23fb3943a594a749
Kenny Lavender <leapingfish@gmail.com>
2018-05-13 10:26:51 -0700

    add deep freeze to avoid accidental mutation

--- src/test/merge-features.js
+++ src/test/merge-features.js
@@ -1,13 +1,28 @@
 import { describe } from 'riteway';
+import deepFreeze from 'deep-freeze';
+
 import { mergeFeatures } from '../merge-features';
 
 describe('mergeFeatures()', async should => {
   const { assert } = should();
+  
+  assert({
+    given: 'one array',
+    should: 'return an empty array',
+    actual: mergeFeatures(
+      deepFreeze([]),
+    ),
+    expected: []
+  });
 
   assert({
     given: 'two or more empty arrays',
     should: 'return an empty array',
-    actual: mergeFeatures([], [], []),
+    actual: mergeFeatures(
+      deepFreeze([]),
+      deepFreeze([]),
+      deepFreeze([])
+    ),
     expected: []
   });
 
@@ -15,9 +30,9 @@ describe('mergeFeatures()', async should => {
     given: 'two or more arrays of strings with duplicate values',
     should: 'return a new array with all the unique values',
     actual: mergeFeatures(
-      ['foo', 'bar', 'baz'],
-      ['bar', 'cat', 'bat'],
-      ['baz', 'rat', 'dog']
+      deepFreeze(['foo', 'bar', 'baz']),
+      deepFreeze(['bar', 'cat', 'bat']),
+      deepFreeze(['baz', 'rat', 'dog'])
     ),
     expected: ['foo', 'bar', 'baz', 'cat', 'bat', 'rat', 'dog']
   });
