db35573c48d005deef32ebe30051dd2c1f287ace
jwicks31 <jessewicks@gmail.com>
2018-05-28 17:43:15 -0500

    adds getBrowserQueryFeatures
    adds getBrowserQueryFeatures

--- src/get-browser-query-features.js
+++ src/get-browser-query-features.js
@@ -2,6 +2,11 @@ import parse from 'url-parse';
 import { parseQuery } from './parse-query';
 
 export const getBrowserQueryFeatures = url => {
-  const { query } = parse(url, true);
-  return parseQuery(query);
+  if (typeof window === 'undefined') {
+    const { query } = parse(url, true);
+    return parseQuery(query);
+  } else {
+    const query = url ? parse(url, true) : parse(window.location.search, true);
+    return parseQuery(query);
+  }
 };
--- src/test/get-browser-query-features.js
+++ src/test/get-browser-query-features.js
@@ -1,23 +1,48 @@
 import { describe } from 'riteway';
 import deepFreeze from 'deep-freeze';
+// import { JSDOM } from 'jsdom';
 import { getBrowserQueryFeatures } from '../get-browser-query-features';
 
 describe('getBrowserQueryFeatures()', async should => {
-  const { assert } = should('return false');
+  const { assert } = should();
   {
-    const url = 'https://domain.com/foo?ft=';
     assert({
-      given: 'no url arguments',
+      given: 'no arguments in node',
+      should: 'return empy array',
+      actual: getBrowserQueryFeatures(),
+      expected: []
+    });
+  }
+  {
+    const url = '?ft=';
+    assert({
+      given: 'search string with no features',
+      should: 'return empy array',
       actual: getBrowserQueryFeatures(deepFreeze(url)),
       expected: []
     });
   }
   {
-    const url = 'https://domain.com/foo?ft=foo,bar';
+    const url = '?ft=foo,bar';
     assert({
-      given: 'url arguments',
+      given: 'search string with features',
+      should: 'return the correct features',
       actual: getBrowserQueryFeatures(deepFreeze(url)),
       expected: ['foo', 'bar']
     });
   }
+  // {
+  //   const dom = new JSDOM(`<script src="../get-browser-query-features">`, {
+  //     url: 'https://example.com/?ft=foo,bar',
+  //     contentType: 'text/html',
+  //     runScripts: 'outside-only',
+  //     resources: 'usable'
+  //   });
+  //   assert({
+  //     given: 'no arguments in browser',
+  //     should: 'return the correct features',
+  //     actual: dom.window.eval('getBrowserQueryFeatures()'),
+  //     expected: ['foo', 'bar']
+  //   });
+  // }
 });
