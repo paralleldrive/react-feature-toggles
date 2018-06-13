7c92871248d4e4191a24a2dc6e5c55f23ae0a26a
jwicks31 <jessewicks@gmail.com>
2018-06-13 18:25:28 -0500

    wip

--- examples/example-usage/src/App.js
+++ examples/example-usage/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { FeatureToggles } from './dist/index';
 import { featureOr404 } from './hocs/featureOr404';
 import { CreateReactAppPage } from './features/CreateReactAppPage';
+import { withButtons } from './hocs/withButtons';
 
 const CreateReactApp = featureOr404('react', CreateReactAppPage);
+const features = ['react'];
 
-export const App = ({ features }) => {
+export const App = withButtons(({ features }) => {
   return (
     <FeatureToggles features={features}>
       <CreateReactApp />
     </FeatureToggles>
   );
-};
+})(features);
--- examples/example-usage/src/index.js
+++ examples/example-usage/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { withButtons } from './hocs/withButtons';
-const features = ['react'];
 
-const Apps = withButtons(App)(features);
-
-ReactDOM.render(<Apps />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
