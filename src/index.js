fa06ea08bbb6575c4eed8a71aa64feee2dc79150
Jesse Wicks <jessewicks@gmail.com>
2018-06-20 15:00:52 -0500

    exports getBrowserQueryFeatures

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ export { createRouteMiddleware } from './create-route-middleware';
 export { withFeatureToggles } from './with-feature-toggles';
 export { getReqQueryFeatures } from './get-req-query-features';
 export { getCurrentActiveFeatures } from './get-current-active-features';
+export { getBrowserQueryFeatures } from './get-browser-query-features';
