87d686a8568e28249bf23afe8e62ca085f28fc31
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-26 17:19:31 -0700

    Update with-feature-toggles.js (#157)

--- src/with-feature-toggles.js
+++ src/with-feature-toggles.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { FeatureToggles } from './feature-toggles';
 
-// ({ features = [...String] } = {}) => Component => Component
-
 export const withFeatureToggles = ({ features } = {}) => Component => props => (
   <FeatureToggles features={features}>
     <Component {...props} />
