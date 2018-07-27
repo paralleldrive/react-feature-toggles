9fcc6ed3c37de262d2e13338a8a8a84101f9b489
Jesse Wicks <jessewicks@Jesses-MacBook-Pro-3.local>
2018-07-26 19:08:55 -0500

    wip

--- src/test/index.js
+++
@@ -1,4 +0,0 @@
-import './configure-feature';
-import './feature-toggles';
-import './feature';
-import './with-feature-toggles';
---
+++ test/index.js
@@ -0,0 +1,4 @@
+import './configure-feature.test';
+import '../feature-toggles.test';
+import '../feature.test';
+import '../with-feature-toggles.test';
