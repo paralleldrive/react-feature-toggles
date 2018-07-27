dd1689bd3188489b25a4a9a1bbe6f40a52abccf5
Jesse Wicks <jessewicks@Jesses-MacBook-Pro-3.local>
2018-07-26 19:30:34 -0500

    updates typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,13 +1,9 @@
 declare module '@paralleldrive/react-feature-toggles' {
   import * as React from 'react';
 
-  interface Feature {
-    readonly name: string;
-    readonly isActive: boolean;
-    readonly dependencies?: ReadonlyArray<string>;
-  }
-
-  type Feature1 = { children?: (features:  ReadonlyArray<string>) => React.ReactNode };
+  type Feature1 = {
+    children?: (features: ReadonlyArray<string>) => React.ReactNode;
+  };
   type Feature2 = {
     inactiveComponent: () => React.ReactNode;
     activeComponent: () => React.ReactNode;
@@ -25,25 +21,8 @@ declare module '@paralleldrive/react-feature-toggles' {
   }): JSX.Element;
   export { FeatureToggles };
 
-  type ConfigureFeature1 = (
-    name: string,
-    activeComponent: React.ReactNode
-  ) => () => JSX.Element;
-  type ConfigureFeature2 = (
-    name: string
-  ) => (activeComponent: React.ReactNode) => () => JSX.Element;
-
   function configureFeature(
     inactiveComponent: React.ReactNode
-  ): ConfigureFeature1 & ConfigureFeature2;
-  function configureFeature(
-    inactiveComponent: React.ReactNode,
-    name: string
-  ): (activeComponent: React.ReactNode) => () => JSX.Element;
-  function configureFeature(
-    inactiveComponent: React.ReactNode,
-    name: string,
-    activeComponent: React.ReactNode
-  ): () => JSX.Element;
+  ): (name: string) => (activeComponent: React.ReactNode) => () => JSX.Element;
   export { configureFeature };
 }
