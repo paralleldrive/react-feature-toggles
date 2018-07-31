ffe043d143a067bda3c8c2e33184fd725653bfb9
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-31 00:32:09 -0700

    fix typings, should not use React.ReactNode

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,34 +1,26 @@
 declare module '@paralleldrive/react-feature-toggles' {
-  import * as React from 'react';
+  type Component = (props: any) => JSX.Element;
 
-  function Feature({
-    children
-  }: {
-    children?: (
-      { features }: { features: ReadonlyArray<string> }
-    ) => React.ReactNode;
-  }): React.Consumer;
-  function Feature({
-    inactiveComponent,
-    name,
-    activeComponent
-  }: {
-    inactiveComponent: () => React.ReactNode;
-    activeComponent: () => React.ReactNode;
+  function Feature(props: {
+    inactiveComponent: Component;
+    activeComponent: Component;
     name: string;
-  }): React.Consumer;
+  }): JSX.Element;
+  function Feature(props: {
+    children: (obj: { features: ReadonlyArray<string> }) => JSX.Element;
+  }): JSX.Element;
+
   export { Feature };
 
-  function FeatureToggles({
-    features
-  }?: {
-    readonly features?: ReadonlyArray<string>;
-    readonly children?: React.ReactNode;
+  function FeatureToggles(props: {
+    readonly features: ReadonlyArray<string>;
+    readonly children: JSX.Element;
   }): JSX.Element;
+
   export { FeatureToggles };
 
   function configureFeature(
-    inactiveComponent: React.ReactNode
-  ): (name: string) => (activeComponent: React.ReactNode) => () => JSX.Element;
+    inactiveComponent: Component
+  ): (name: string) => (activeComponent: Component) => Component;
   export { configureFeature };
 }
