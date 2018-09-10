4b5fa11ed404ab11df163064eaed0b1d3cf60566
Jesse Wicks <33884732+jwicks31@users.noreply.github.com>
2018-09-10 15:52:30 -0500

    Updates StatelessComponent to SFC in typings (#177)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,7 @@
 declare module '@paralleldrive/react-feature-toggles' {
-  import { ComponentClass, StatelessComponent } from 'react';
+  import { ComponentClass, SFC, StatelessComponent } from 'react';
   type FeatureNames = ReadonlyArray<string>;
-  type Component = ComponentClass | StatelessComponent;
+  type Component = ComponentClass | SFC<any> | StatelessComponent;
 
   export function Feature(props: {
     children: (args: { features: FeatureNames }) => JSX.Element;
