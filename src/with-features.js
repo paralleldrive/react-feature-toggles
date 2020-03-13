0f31001d907e9256de91f685fd4f7238e7cda59d
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2020-03-12 23:36:07 -0700

    prettier

--- src/with-features.js
+++ src/with-features.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { FeatureTogglesContext } from './context';
 
 export const withFeatures = Component => props => {
-	return (
-		<FeatureTogglesContext.Consumer>
-			{features => (
-				<Component {...props} features={features}/>
-			)}
-		</FeatureTogglesContext.Consumer>
-	);
+  return (
+    <FeatureTogglesContext.Consumer>
+      {features => <Component {...props} features={features} />}
+    </FeatureTogglesContext.Consumer>
+  );
 };
